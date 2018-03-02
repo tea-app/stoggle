93521800d50075276b21b3428546e4d692b33d0d
yasuhira <yasuhirap@gmail.com>
2018-03-02 23:53:31 +0900

    Create top page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Button from 'material-ui/Button'
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
+import deepOrange from 'material-ui/colors/deepOrange'
+import Top from './pages/Top'
+import purple from 'material-ui/colors/purple'
 
 const rootElement = document.querySelector('#root')
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secodary: deepOrange,
+  }
+})
 
-function App() {
-  return (
-    <Button variant="raised" color="primary">
-      Hello,stoggle
-    </Button>
-  );
+const Stoggle = () => (
+  <MuiThemeProvider theme={theme}>
+    <Top />
+  </MuiThemeProvider>
+)
+
+if(rootElement){
+  render(<Stoggle />, rootElement)
 }
-
-render(<App />, rootElement)
